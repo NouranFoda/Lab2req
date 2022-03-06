ec167ea864448df8c8b35d490861e8136a8dafbb
NouranFoda <nouran2000.foda@gmail.com>
2022-03-06 17:55:13 +0200

    initial upload

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request
+
+# TODO: create new Flask app
+app=Flask(__name__)
+
+users=[ 
+    { 
+        "email":"nouran2000.foda@gmail.com",
+        "username":"Nouran",
+        "password":123
+    },
+    {
+        "email":"farouha.shawky@gmail.com",
+        "username":"Farah",
+        "password":1234
+    }
+]
+
+def sign_up():
+    # TODO: get user input from request
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password = request.form.get("password")
+   
+    if not user_exists(email=email, username=username, password=password):
+        return "<h2>New user has been created</h2>"
+    else:
+        return "<h2>This user already exists</h2>"
+
+def user_exists(email, username, password):
+    for user in users:
+        if (user["email"]==email and user["username"]==username):
+            return True
+    return False
+
+@app.route("/")
+def main_page():
+    # TODO: return index.html
+    return render_template("index.html")
+    
+# TODO: Add route for sign up
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    return sign_up()
+   
+    
+
+app.run(debug=True)
+
+
+
+
---
+++ static/js/utils.js
@@ -0,0 +1,5 @@
+function alert_welcome() {
+	// TODO: show alert with welcoming message
+	// HINT: check alert()
+	alert("welcome to the signup page")
+}
